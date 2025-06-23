590ea1d19af781428a451dacd47d03117cab6a2e
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-23 10:06:22 +0200

    Add test cases for safe comparison of integral numbers

--- compare/integer_comparisons.cpp
+++ compare/integer_comparisons.cpp
@@ -9,5 +9,32 @@ TEST_CASE("safe comparing integral numbers")
     int x = -42;
     unsigned int y = 665;
 
-    //CHECK(x < y);
-}
+    SECTION("direct comparison")
+    {
+        //REQUIRE(x < y);
+        REQUIRE(std::cmp_less(x, y));
+    }
+
+    SECTION("with generic functions")
+    {
+        auto my_comparer = [](auto a, auto b) { 
+            if constexpr(std::is_integral_v<decltype(a)> && std::integral<decltype(b)>)
+                return std::cmp_less(a, b);
+            else
+                return a < b;
+        };
+
+        REQUIRE(my_comparer(x, y));
+
+        std::string str1 = "abc";
+        std::string str2 = "def";
+
+        REQUIRE(my_comparer(str1, str2));
+    }
+
+    SECTION("in_range")
+    {
+        REQUIRE(std::in_range<uint8_t>(x) == false);
+        REQUIRE(std::in_range<uint8_t>(42) == true);
+    }
+} 
