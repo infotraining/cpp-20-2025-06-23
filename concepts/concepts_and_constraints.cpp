e2b743dd19d7077d16145c5d225597e2708ad199
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-23 16:04:08 +0200

    Refactor type traits and add Pointer concept with max_value function overloads

--- concepts/concepts_and_constraints.cpp
+++ concepts/concepts_and_constraints.cpp
@@ -1,38 +1,178 @@
 #include <catch2/catch_test_macros.hpp>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
 #include <concepts>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
 
 using namespace std::literals;
+
+// type trait - IsPointer_v
+
+template <typename T>
+struct IsPointer : std::false_type
+{
+    // static constexpr bool value = false;
+};
+
+template <typename T>
+struct IsPointer<T*> : std::true_type
+{
+    // static constexpr bool value = true;
+};
+
+// template variable
+template <typename T>
+constexpr inline bool IsPointer_v = IsPointer<T>::value;
+
+TEST_CASE("type traits")
+{
+    using T1 = int;
+    using T2 = int*;
+    using T3 = const int*;
+    using T4 = volatile int*;
+
+    static_assert(not IsPointer<T1>::value);
+    static_assert(IsPointer_v<T2>);
+    static_assert(std::is_pointer_v<T2>);
+    static_assert(IsPointer_v<T3>);
+    static_assert(IsPointer_v<T4>);
+}
 
 template <typename TContainer>
 void print(TContainer&& c, std::string_view prefix = "items")
 {
     std::cout << prefix << ": [ ";
-    for(const auto& item : c)
+    for (const auto& item : c)
         std::cout << item << " ";
     std::cout << "]\n";
 }
 
-template <typename T>
-T max_value(T a, T b)
+namespace Ver_1
 {
-    return a < b ? b : a;
-}
+    template <typename T>
+    T max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename T>
+        requires IsPointer_v<T>
+    auto max_value(T a, T b)
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return max_value(*a, *b);
+    }
+} // namespace Ver_1
+
+namespace Ver_2
+{
+    template <typename T>
+    concept Pointer = IsPointer_v<T>;
+
+    static_assert(IsPointer_v<int*>);
+    static_assert(Pointer<int*>);
+    static_assert(Pointer<const int*>);
+    static_assert(not Pointer<int>);
+
+    template <typename T>
+    T max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <typename T>
+        requires Pointer<T>
+    auto max_value(T a, T b)
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return max_value(*a, *b);
+    }
+} // namespace Ver_2
+
+template <typename T>
+concept Pointer = requires(T ptr) {
+    *ptr;
+    ptr == nullptr;
+    ptr != nullptr;
+};
+
+inline namespace Ver_3
+{
+    template <typename T>
+    T max_value(T a, T b)
+    {
+        return a < b ? b : a;
+    }
+
+    template <Pointer T>
+    auto max_value(T a, T b)
+    {
+        assert(a != nullptr);
+        assert(b != nullptr);
+        return max_value(*a, *b);
+    }
+} // namespace Ver_3
+
+// inline namespace Ver_4
+// {
+//     template <typename T>
+//     T max_value(T a, T b)
+//     {
+//         return a < b ? b : a;
+//     }
+
+//     auto max_value(auto a, auto b) -> decltype(*a)
+//         requires (Pointer<decltype(a)> && Pointer<decltype(b)> && std::same_as<decltype(a), decltype(b)>)
+//     {
+//         assert(a != nullptr);
+//         assert(b != nullptr);
+//         return *a < *b ? *b : *a;
+//     }
+// }
+
+// template <Pointer T1, Pointer T2>
+//     requires std::same_as<T1, T2>
+// auto max_value(T1 a, T2 b)
+// {
+//     assert(a != nullptr);
+//     assert(b != nullptr);
+//     return max_value(*a, *b);
+// }
+
 
 TEST_CASE("constraints")
 {
     int x = 10;
     int y = 20;
 
-    CHECK(max_value(x, y) == 20);
+    //CHECK(max_value(x, y) == 20);
+    CHECK(max_value(&x, &y) == 20);
 
-    //CHECK(max_value(&x, &y) == 20);
+    const int* ptr_to_const_1 = &x;
+    const int* ptr_to_const_2 = &y;
+    CHECK(max_value(ptr_to_const_1, ptr_to_const_2) == 20);
+
+    auto sptr1 = std::make_shared<int>(20);
+    auto sptr2 = std::make_shared<int>(42);
+    CHECK(max_value(sptr1, sptr2) == 42);
 }
+
+template <typename T>
+struct Wrapper
+{
+    T value;
+
+    void print() const
+    {
+        std::cout << "value: " << value << "\n";
+    }
+};
 
 TEST_CASE("concepts")
 {
-    REQUIRE(true);
+    Wrapper<int> w1{42};
+    w1.print();
 }
--- templates/templates.cpp
+++ templates/templates.cpp
@@ -1,9 +1,62 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <set>
 
 using namespace std::literals;
+
+auto add(auto a, auto b)
+{
+    return a + b;
+}
+
+namespace Explain
+{
+    template <typename T1, typename T2>
+    auto add(T1 a, T2 b)
+    {
+        return a + b;
+    }
+} // namespace Explain
+
+void add_to(auto& container, auto&& item)
+{
+    //undefined(); // phase 1
+    //container.undefined(item); // phase 2
+
+    container.push_back(std::forward<decltype(item)>(item));
+}
+
+namespace AlternativeTake
+{
+    template <typename TItem>
+    void add_to(auto& container, TItem&& item)
+    {
+        container.push_back(std::forward<TItem>(item));
+    }
+}
+
+namespace Explain
+{
+    template <typename T1, typename T2>
+    void add_to(T1& container, T2&& item)
+    {
+        container.push_back(std::forward<decltype(item)>(item));
+    }
+} // namespace Explain
+
+TEST_CASE("functions with auto params")
+{
+    int x = 12;
+    int y = 777;
+    REQUIRE(add(x, y) == 789);
+
+    std::vector<int> vec = {1, 2, 3, 4};
+    add_to(vec, 5);
+
+    // REQUIRE(vec == std::vector{1, 2, 3, 4, 5});
+}
 
 TEST_CASE("templates & lambda expressions")
 {
