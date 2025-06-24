adf3d62cdb25272b0ee8dd30dc695083e04733ad
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-24 15:57:11 +0200

    span & ranges

--- ranges/ranges_and_views.cpp
+++ ranges/ranges_and_views.cpp
@@ -7,6 +7,12 @@
 
 using namespace std::literals;
 
+struct Person
+{
+    int id;
+    std::string name;
+};
+
 TEST_CASE("ranges", "[ranges]")
 {
     auto data = helpers::create_numeric_dataset<20>(42);
@@ -18,17 +24,33 @@ TEST_CASE("ranges", "[ranges]")
 
     SECTION("algorithms")
     {
-        // TODO
+        std::sort(words.begin(), words.end());
+        std::ranges::sort(words, std::greater{});
+
+        helpers::print(words, "words");
     }
 
     SECTION("projections")
     {
-        // TODO        
+        //std::sort(words.begin(), words.end(), [](const auto& left, const auto& right) { return left.size() < right.size(); });
+        std::ranges::sort(words, std::less{}, &std::string::size);
+        helpers::print(words, "words");
+
+        std::vector<Person> people = { Person{42, "Jan"}, {55, "Adam"}, {88, "Zenon"} };
+        std::ranges::sort(people, std::less{}, &Person::name); 
+        for(const auto& p : people)
+            std::cout << p.name << " ";
+        std::cout << "\n";
     }
 
     SECTION("concepts & tools")
     {
-        // TODO
+        std::vector vec = {1, 2, 3};
+
+        using T = std::ranges::range_value_t<decltype(vec)>;
+        using Iterator = std::ranges::iterator_t<decltype(vec)>;
+
+        std::sort(std::ranges::begin(vec), std::ranges::end(vec));
     }
 }
 
@@ -43,11 +65,16 @@ struct EndValue
 
 TEST_CASE("sentinels", "[ranges]")
 {
-    std::vector data = {2, 3, 4, 1, 5, 42, 6, 7, 8, 9, 10};
-
-    // TODO - sort range [begin; 42) in descending order
-
+    std::vector data = {2, 3, 4, 1, 5, 42, 6, 9, 8, 11, 10, 7};
+    std::ranges::sort(data.begin(), EndValue<42>{});
     helpers::print(data, "data");
+
+    auto pos = std::ranges::find(data.begin(), std::unreachable_sentinel, 42);
+    REQUIRE(*pos == 42);
+
+    char txt[] = { 'a', 'b', 'c', '\0', 'e', 'f' };
+    std::ranges::sort(std::ranges::begin(txt), EndValue<'\0'>{}, std::greater{});
+    helpers::print(txt, "txt");
 }
 
 TEST_CASE("views")
@@ -56,10 +83,22 @@ TEST_CASE("views")
 
     SECTION("all")
     {
+        auto all_view = std::views::all(data);
+        auto target = all_view; // O(1)
+        helpers::print(target, "target");
     }
 
     SECTION("subrange - iterator & sentinel as a view")
     {
+        auto head = std::ranges::subrange{data.begin(), EndValue<42>{}};
+        std::ranges::sort(head);
+        helpers::print(data, "data");
+
+        for(auto& item : head)
+            item = 0;
+
+        head[3] = 665;
+        helpers::print(data, "data");
     }
 
     SECTION("counted")
--- stdlib-features/std_lib_cpp20.cpp
+++ stdlib-features/std_lib_cpp20.cpp
@@ -2,10 +2,146 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <span>
+#include <numbers>
+#include <array>
+#include <mdspan>
+#include <print>
 
 using namespace std::literals;
 
+void print(std::span<const int> data, std::string_view desc)
+{
+    std::cout << desc << ": [ ";
+    for(const auto& item : data)
+        std::cout << item << " ";
+    std::cout << "]\n";
+}
+
+void zero(std::span<int> data, int default_value = 0)
+{
+    for(auto& item : data)
+        item = default_value;
+}
+
 TEST_CASE("std::span")
 {
-    REQUIRE(true);
+    std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    SECTION("fixed extent")
+    {
+        std::span<int, 5> spn_1{vec.begin(), 5}; 
+        print(spn_1, "spn_1");
+    }
+
+    SECTION("dynamic extent")
+    {
+        std::span<int> spn_2{vec};
+        print(spn_2, "spn_2");
+
+        spn_2 = std::span{vec.begin() + 2, 3};
+        zero(spn_2);
+
+        spn_2[0] = 665;
+
+        print(vec, "vec");
+    }
+}
+
+void print_as_bytes(const float f, const std::span<const std::byte> bytes)
+{
+#ifdef __cpp_lib_format
+    std::cout << std::format("{:+6}", f) << " - { ";
+
+    for (std::byte b : bytes)
+    {
+        std::cout << std::format("{:02X} ", std::to_integer<int>(b));
+    }
+
+    std::cout << "}\n";
+#endif
+}
+
+TEST_CASE("float as span of bytes")
+{
+    float data[] = {std::numbers::pi_v<float>};
+
+    std::span<const std::byte> const_bytes = std::as_bytes(std::span{data});
+    print_as_bytes(data[0], const_bytes);
+
+    std::span<std::byte> writeble_bytes = std::as_writable_bytes(std::span{data});
+    writeble_bytes[3] |= std::byte{0b1000'0000};
+    print_as_bytes(data[0], const_bytes);
+}
+
+TEST_CASE("why span")
+{
+    int tab[10] = {1, 2, 3, 4};
+    print(tab, "tab");
+
+    int* dynamic_tab = new int[10] { 1, 2, 3, 4 };
+    print(std::span{dynamic_tab, 10}, "dynamic_tab");
+
+    std::vector<int> vec{1, 2, 3, 4};
+    print(vec, "vec");
+
+    std::array<int, 10> arr = {1, 2, 3};
+    print(arr, "arr");
+
+    delete[] dynamic_tab;
+}
+
+//////////////////////////////////////////////////
+// BEWARE DANGLING POINTERS
+
+constexpr std::span<int> get_head(std::span<int> items, size_t head_size = 1)
+{
+    return items.first(head_size);
+}
+
+TEST_CASE("beware dangling pointers")
+{
+    SECTION("OK")
+    {
+        std::vector<int> vec = {1, 2, 3, 4, 5, 6};
+        auto head = get_head(vec, 3);
+        print(head, "head");
+    }
+
+    SECTION("UB")
+    {
+        std::vector<int> vec = {1, 2, 3, 4, 5, 6};
+        auto head = get_head(vec, 3);
+
+        vec.push_back(7);
+
+        print(head, "head"); // UB
+    }
+}
+
+TEST_CASE("mdspan")
+{
+    std::vector v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+ 
+    // View data as contiguous memory representing 2 rows of 6 ints each
+    auto ms2 = std::mdspan(v.data(), 2, 6);
+    // View the same data as a 3D array 2 x 3 x 2
+    auto ms3 = std::mdspan(v.data(), 2, 3, 2);
+ 
+    // Write data using 2D view
+    for (std::size_t i = 0; i != ms2.extent(0); i++)
+        for (std::size_t j = 0; j != ms2.extent(1); j++)
+            ms2[i, j] = i * 1000 + j;
+ 
+    // Read back using 3D view
+    for (std::size_t i = 0; i != ms3.extent(0); i++)
+    {
+        std::println("slice @ i = {}", i);
+        for (std::size_t j = 0; j != ms3.extent(1); j++)
+        {
+            for (std::size_t k = 0; k != ms3.extent(2); k++)
+                std::print("{} ", ms3[i, j, k]);
+            std::println("");
+        }
+    }
 }
